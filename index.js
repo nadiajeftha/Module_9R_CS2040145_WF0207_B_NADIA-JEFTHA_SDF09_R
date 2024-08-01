f5fe59ed6e0d2227896d71cf8d305ea5600d9f5c
Nadia Jeftha <nadiajeftha2@gmail.com>
2024-08-01 14:41:23 +0200

    link to css from html

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
-
-// 2. Create a variable, sum, and set it to the sum of the two cards
-
 let firstCard = 3
 let secondCard = 10
-
 let sum = firstCard + secondCard
-
-console.log(sum)
+let hasBlackJack = false
+let isAlive = true
+let message = ""
 
 if (sum <= 20 ) {
-    console.log("Do you want to draw a new card?  🃏")
+    message ="Do you want to draw a new card?  🃏"
 } else if (sum === 21) {
-    console.log("Good job! You've got BLACKJACK! 🏅")
+    message = "Good job! You've got BLACKJACK! 🏅" 
+    hasBlackJack = true
 } else {
-    console.log("You lost! 😞 Try again next time. ")
+    message = "You lost! 😞 Try again next time. "
+    isAlive = false
 }
+
+console.log (message)
