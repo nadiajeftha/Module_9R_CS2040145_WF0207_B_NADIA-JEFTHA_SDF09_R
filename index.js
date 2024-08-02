b0f1332566b6dd488949bcb33bc19544136aff50
Nadia Jeftha <nadiajeftha2@gmail.com>
2024-08-02 11:41:18 +0200

    added the <script> at the end of the body

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ let sum = 0
 let hasBlackJack = false
 let isAlive = false
 let message = ""
-let messageEl = document.geyElementById("message-el")
+let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
-let playerEl = document.get.ElementById("player-el")
+let playerEl = document.getElementById("player-el")
     
 playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {
-    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    let randomNumber = Math.floor( Math.random() * 13 ) + 1
     if (randomNumber > 10) {
         return 10
     } else if (randomNumber === 1) {
